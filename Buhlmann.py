2073f085e0f91c4dc9bbd5ed6ed5b0fae8f8a3af
Dan Roozemond <dan.roozemond@gmail.com>
2020-03-22 20:06:57 +0100

    Rethinking how to format tissues

--- Buhlmann.py
+++ Buhlmann.py
@@ -3,24 +3,31 @@ import Gas, BuhlmannConstants;
 """
 Buhlmann class contains all essential logic for deco model
 """
+
+
 class Buhlmann:
     # TODO: This is where gradient factors should eventually be added
     def __init__(self):
-        self._halftimes = { 'N2': BuhlmannConstants.ZHL_16C_N2_HALFTIMES,
-                            'He': BuhlmannConstants.ZHL_16C_HE_HALFTIMES };
+        self._n_tissues = BuhlmannConstants.ZHL_16C_N_TISSUES;
+        self._halftimes = {'N2': BuhlmannConstants.ZHL_16C_N2_HALFTIMES,
+                           'He': BuhlmannConstants.ZHL_16C_HE_HALFTIMES};
+        self._coeffs = {'N2': {'a': BuhlmannConstants.ZHL_16C_N2_A_VALUES,
+                               'b': BuhlmannConstants.ZHL_16C_N2_B_VALUES},
+                        'He': {'a': BuhlmannConstants.ZHL_16C_HE_A_VALUES,
+                               'b': BuhlmannConstants.ZHL_16C_HE_B_VALUES}
+                        };
 
     """
-    TissueState is represented as a dict mapping int to float (half time to pp)
-    N2 and He have separate states
+    TissueState is represented as a list of current tissue loadings (N2, He)
     """
-    def init_tissue_state(self, inert_gas):
+    def init_tissue_state(self):
         gas = Gas.Air();
-        return {ht: gas[ 'f' + inert_gas ] for ht in self._halftimes[inert_gas]};
+        return [ (gas['fN2'], gas['fHe'] ) for i in range(self._n_tissues) ];
 
 
-##
-## Some testing functions
-##
+#
+# Some testing functions
+#
 bm = Buhlmann();
-ts = bm.init_tissue_state('N2');
+ts = bm.init_tissue_state();
 print(ts);
