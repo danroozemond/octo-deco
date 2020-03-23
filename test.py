ac82b338d4eeaa7563c89441da9b2abfac5a7d27
Dan Roozemond <dan.roozemond@gmail.com>
2020-03-23 21:42:20 +0100

    Oopsie

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
-# dp = DiveProfile();
-# dp.append_section(40, 35, gas = Gas.Air());
-# dp.append_section(30, 20);
-# dp.append_surfacing();
-#
-# dp.interpolate_points();
-# dp.update_deco_info();
-# print(dp.dataframe());
+import Gas;
+from DiveProfile import DiveProfile;
+
+dp = DiveProfile();
+dp.append_section(40, 35, gas = Gas.Air());
+dp.append_section(30, 20);
+dp.append_surfacing();
+
+dp.interpolate_points();
+dp.update_deco_info();
+print(dp.dataframe());
 
