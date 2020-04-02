4c79aa8d24c64de3300b31f55b0d373ef31bb2c1
Dan Roozemond <dan.roozemond@gmail.com>
2020-04-02 09:08:04 +0200

    (cherry-picked) Two small bugs in DiveProfile

--- DiveProfile.py
+++ DiveProfile.py
@@ -141,7 +141,7 @@ class DiveProfile:
         self.update_deco_info();
 
     def _fix_all_points_prev(self):
-        for i in range(1, self._points):
+        for i in range(1, len(self._points)):
             self._points[i].prev = self._points[i-1];
 
     def uninterpolate_points(self):
@@ -164,7 +164,7 @@ class DiveProfile:
         for p in self._points:
             p.set_updated_deco_info( self._deco_model, self._gases_carried, amb_to_gf = amb_to_gf );
             amb_to_gf = p.deco_info['amb_to_gf'];
-        self._desc_deco_model_profile = self._deco_model.description();
+        self._desc_deco_model_display = self._deco_model.description();
         self._full_info_computation_time = time.perf_counter() - t0;
 
     '''
