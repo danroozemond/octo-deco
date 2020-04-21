48defdbb474e4c8ba216417818f8ce47143b7798
Dan Roozemond <dan.roozemond@gmail.com>
2020-04-21 11:19:41 +0200

    Fix behaviour of removing deco stops / interpolated points

--- octodeco/test.py
+++ octodeco/test.py
@@ -1,19 +1,23 @@
 # Please see LICENSE.md
 
-from deco import Gas, Util, CreateDive;
-from deco.DiveProfile import DiveProfile;
+from octodeco.deco import Gas, Util, CreateDive;
+from octodeco.deco.DiveProfile import DiveProfile;
 
-# dp = DiveProfile();
-# dp.append_section(40, 35, gas = Gas.Air());
-# dp.append_section(30, 20);
-# dp.append_surfacing();
-# dp.add_stops();
-# dp.interpolate_points();
-# dp.update_deco_info();
-# print(dp.dataframe());
-
-dp = CreateDive.create_from_shearwater_csv_file("C:/Users/Dan Roozemond/Desktop/30189285#219_2020-01-05.csv");
-print(dp.description());
+dp = DiveProfile();
+dp.append_section(50, 50, gas = Gas.Trimix(11,40));
+dp.append_section(30, 50, gas = Gas.Trimix(21,35));
+dp.add_gas(Gas.Nitrox(50));
+dp.add_gas(Gas.Nitrox(99));
+dp.add_stops_to_surface();
+dp.interpolate_points();
 
 for p in dp._points:
-    print(p.time, p.depth, p.deco_info['SurfaceGF'])
+    if p.time >= 50 and p.time <= 60:
+        print(p.time, p.depth, p.is_deco_stop, p.is_interpolated_point, p.deco_info['SurfaceGF'])
+
+dp.set_gf(60,100);
+dp.update_stops();
+print('\nwith 60/100');
+for p in dp._points:
+    if p.time >= 50 and p.time <= 60:
+        print(p.time, p.depth, p.deco_info['SurfaceGF'])
