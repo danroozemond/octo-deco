757e0bd9621755fce0ccc35040d4cbc4ec5c3637
Dan Roozemond <dan.roozemond@gmail.com>
2020-04-30 16:14:58 +0200

    updated_state. So far ugliness increases very quickly; hopefully speed increases accordingly...

--- octodeco/deco/TissueStateCython.pyx
+++ octodeco/deco/TissueStateCython.pyx
@@ -1,42 +1,97 @@
 # Please see LICENSE.md
+array;
+import array;
+
 from . import Gas;
 
+CONSTANT_ID = None;
+cdef int N_TISSUES = 16;
+cdef float[32] HALFTIMES;
+cdef float[32] A_VALUES;
+cdef float[32] B_VALUES;
 
 # TissueState is represented as a list of current tissue loadings (N2, He)
 class TissueState:
-    def __init__(self, constants):
-        gas = Gas.Air();
-        self._constants = constants;
-        self._n_tissues = self._constants.N_TISSUES;
-        self._state = list(map( lambda i: (gas[ 'fN2' ], gas[ 'fHe' ]), range(self._n_tissues)));
+    def __init__(self, constants, empty=False):
+        if constants is not None:
+            TissueState.ensure_cython_constants(constants);
+        self._ab = None;
+        if not empty:
+            self._set_init_state();
 
     def __repr__(self):
         return self._state.__repr__();
 
-    def __copy__(self):
-        r = TissueState(self._constants);
-        r._state = self._state;
-        return r;
+    def _set_init_state(self):
+        gasc = TissueState.gas_to_cython(Gas.Air());
+        self._state = array.array('f',[]); array.resize(self._state, 2*N_TISSUES);
+        cdef float[:] cstate = self._state;
+        cdef int i = 0, n = 16;
+        while i < N_TISSUES:
+            cstate[2*i] = gasc[0];
+            cstate[2*i+1] = gasc[1];
+            i += 1;
 
-    def copy(self):
-        return self.__copy__();
+
+    #
+    # Storing the constants in cython format
+    #
+    @staticmethod
+    def _create_cython_constants(constants):
+        global CONSTANT_ID;
+        CONSTANT_ID = constants.ID;
+        assert N_TISSUES == constants.N_TISSUES;
+        cdef int i = 0;
+        while i < N_TISSUES:
+            # N2
+            HALFTIMES[2*i] = constants.N2_HALFTIMES[i];
+            A_VALUES[2*i]  = constants.N2_A_VALUES[i];
+            B_VALUES[2*i]  = constants.N2_B_VALUES[i];
+            #He
+            HALFTIMES[2*i+1] = constants.HE_HALFTIMES[i];
+            A_VALUES[2*i+1]  = constants.HE_A_VALUES[i];
+            B_VALUES[2*i+1]  = constants.HE_B_VALUES[i];
+            # Next
+            i += 1;
+
+    @staticmethod
+    def ensure_cython_constants(constants):
+        if CONSTANT_ID is None:
+            TissueState._create_cython_constants(constants);
+        # Only support one set of constants at run time
+        assert CONSTANT_ID == constants.ID;
+
+    @staticmethod
+    def _create_gas_to_cython(gas):
+        cdef float r[2];
+        r[0] = gas['fN2'];
+        r[1] = gas['fHe'];
+        gas['cython_array'] = r;
+
+    @staticmethod
+    def gas_to_cython(gas):
+        if 'cython_array' not in gas:
+            TissueState._create_gas_to_cython(gas);
+        return gas['cython_array'];
 
     #
     # Updating the tissue states
     #
-    @staticmethod
-    def _updated_partial_pressure(pp_tissue:float, pp_alveolar:float, halftime:float, duration:float):
-        return pp_tissue + (1 - pow(.5, duration / halftime)) * (pp_alveolar - pp_tissue);
-
     def updated_state(self, duration, p_amb, gas):
-        cdef float pp_amb_n2, pp_amb_he;
-        r = self.copy();
-        pp_amb_n2 = p_amb * gas[ 'fN2' ];
-        pp_amb_he = p_amb * gas[ 'fHe' ];
-        r._state = [ (
-            self._updated_partial_pressure(self._state[i][0], pp_amb_n2, self._constants.N2_HALFTIMES[ i ], duration),
-            self._updated_partial_pressure(self._state[i][1], pp_amb_he, self._constants.HE_HALFTIMES[ i ], duration)
-        ) for i in range(self._n_tissues) ];
+        r = TissueState(None, empty=True);
+        r._state = array.array('f',[]); array.resize(r._state, 2*N_TISSUES);
+        cdef float[:] coldstate = self._state;
+        cdef float[:] cnewstate = r._state;
+        gasc = TissueState.gas_to_cython(gas);
+        cdef float pp_amb_n2 = p_amb * gasc[0];
+        cdef float pp_amb_he = p_amb * gasc[1];
+        cdef float cdur = duration;
+        cdef int i = 0;
+        cdef float tmp;
+        while i < N_TISSUES:
+            cnewstate[2*i] = coldstate[2*i] + ( 1 - 0.5 **( cdur/HALFTIMES[2*i]) ) * ( pp_amb_n2 - coldstate[2*i] );
+            cnewstate[2*i+1] = coldstate[2*i+1] + ( 1 - 0.5 ** ( cdur/HALFTIMES[2*i+1]) ) * ( pp_amb_he - coldstate[2*i+1] );
+            i += 1;
         return r;
 
     #
