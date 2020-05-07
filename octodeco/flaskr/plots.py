52ea611475872ba6cbec085d790ee861a1974914
Dan Roozemond <dan.roozemond@gmail.com>
2020-05-07 13:26:10 +0200

    Plot "1st stop" first, so that it is in the background

--- octodeco/flaskr/plots.py
+++ octodeco/flaskr/plots.py
@@ -13,6 +13,21 @@ def show_diveprofile(diveprofile):
     # Later, for deco info, see filled lines here: https://plot.ly/python/line-charts/
     #
     fig = sp.make_subplots(specs = [ [ {"secondary_y": True} ] ])
+    # A bit complicated code for adding the shaded area
+    fs_xs = list(df[ "time" ]);
+    fs_ys1 = list(df[ "FirstStop" ]);
+    fs_ys2 = [ 0 for y in fs_ys1 ];
+    comp_x = fs_xs + [ fs_xs[ -1 ], fs_xs[ -1 ] ] + fs_xs[ ::-1 ];
+    comp_y = fs_ys2 + [ 0, fs_ys1[ -1 ] ] + fs_ys1[ ::-1 ];
+    fig.add_trace(go.Scatter(
+        x = comp_x,
+        y = comp_y,
+        fill = 'toself',
+        fillcolor = 'rgba(255,0,0,0.2)',
+        line_color = 'rgba(255,0,0,0)',
+        showlegend = True,
+        name = '1st stop',
+    ))
     fig.add_trace(go.Scatter(x = df[ "time" ], y = 100 * df[ "ppO2" ], name = 'ppO2',
                              line = {'color': 'rgb(176,42,143)', 'dash': 'dot', 'width': 1},
                              visible = "legendonly"),
@@ -32,21 +47,7 @@ def show_diveprofile(diveprofile):
     # fig.add_trace( go.Scatter( x=df["time"], y=(100/16)*(df["LeadingTissueIndex"]+2), name='Leading tissue',
     #                            line={'color': 'rgb(251,165,56)', 'dash': 'dot', 'width': 1} ),
     #                secondary_y = True );
-    # A bit complicated code for adding the shaded area
-    fs_xs = list(df[ "time" ]);
-    fs_ys1 = list(df[ "FirstStop" ]);
-    fs_ys2 = [ 0 for y in fs_ys1 ];
-    comp_x = fs_xs + [ fs_xs[ -1 ], fs_xs[ -1 ] ] + fs_xs[ ::-1 ];
-    comp_y = fs_ys2 + [ 0, fs_ys1[ -1 ] ] + fs_ys1[ ::-1 ];
-    fig.add_trace(go.Scatter(
-        x = comp_x,
-        y = comp_y,
-        fill = 'toself',
-        fillcolor = 'rgba(255,0,0,0.2)',
-        line_color = 'rgba(255,0,0,0)',
-        showlegend = True,
-        name = '1st stop',
-    ))
+
     #  Set some axes parameters; draw
     fig.update_yaxes(secondary_y = False, autorange = "reversed");
     fig.update_yaxes(secondary_y = True, showgrid = False, range = [ -1, 140 ], tick0 = 0, dtick = 20);
