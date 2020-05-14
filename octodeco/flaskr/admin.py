294f281466d6bbd70b844b1c8a1a4c9b07e61a50
Dan Roozemond <dan.roozemond@gmail.com>
2020-05-14 20:44:55 +0200

    admin: plumbing

---
+++ octodeco/flaskr/admin.py
@@ -0,0 +1,28 @@
+# Please see LICENSE.md
+from flask import (
+    Blueprint, render_template, redirect, flash, url_for, abort
+)
+
+from . import db_dive, user;
+
+bp = Blueprint('admin', __name__, url_prefix='/admin')
+
+
+@bp.before_request
+def load_user_details():
+    ud = user.get_user_details();
+    if not ud.is_admin:
+        abort(403);
+
+
+@bp.route('/')
+@bp.route('/info')
+def info():
+    return render_template('admin/info.html');
+
+
+@bp.route('/update', methods = [ 'POST' ])
+def update():
+    return 'hello, world';
+
+
--- octodeco/flaskr/user.py
+++ octodeco/flaskr/user.py
@@ -22,6 +22,10 @@ class UserDetails(dict):
         return self['google_sub'] is not None and self['google_sub'] != '';
 
     @property
+    def is_admin(self):
+        return self['is_admin'] == 1;
+
+    @property
     def login_link(self):
         return auth.get_google_request_uri();
 
