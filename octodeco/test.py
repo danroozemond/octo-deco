3a0b395e0f7e347f838f9a8f805164c354b6cb2f
Dan Roozemond <dan.roozemond@gmail.com>
2020-05-23 14:50:49 +0200

    test.py back to old example

--- octodeco/test.py
+++ octodeco/test.py
@@ -7,13 +7,13 @@ from octodeco.deco.DiveProfile import DiveProfile;
 
 t0 = time.perf_counter();
 
-dp = DiveProfile(gf_low=75, gf_high=85);
-dp.append_section(30, 18, gas = Gas.Nitrox(32));
+dp = DiveProfile(gf_low=35, gf_high=65);
+dp.append_section(30, 33.5, gas = Gas.Air());
 dp.add_gas(Gas.Nitrox(50))
 dp.add_stops_to_surface();
-dp.interpolate_points()
+#dp.interpolate_points()
 
-dp.set_gf(35,65, updateStops = True);
+#dp.set_gf(35,65, updateStops = True);
 dp.remove_points(lambda x: x.is_interpolated_point, fix_durations = False, update_deco_info = True)
 
 print(dp.dive_summary());
