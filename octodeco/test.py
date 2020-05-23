6ccc09b30c3d22daa620a0b2876f8fab874ee3d9
Dan Roozemond <dan.roozemond@gmail.com>
2020-05-23 14:23:22 +0200

    check: works for seeing that a no-stop dive needs stop once more conservative GF's are set

--- octodeco/test.py
+++ octodeco/test.py
@@ -7,13 +7,13 @@ from octodeco.deco.DiveProfile import DiveProfile;
 
 t0 = time.perf_counter();
 
-dp = DiveProfile(gf_low=5, gf_high=65);
-dp.append_section(50, 50.5, gas = Gas.Air());
+dp = DiveProfile(gf_low=75, gf_high=85);
+dp.append_section(30, 18, gas = Gas.Nitrox(32));
 dp.add_gas(Gas.Nitrox(50))
 dp.add_stops_to_surface();
 dp.interpolate_points()
 
-# dp.set_gf(0,0, updateStops = False);
+dp.set_gf(35,65, updateStops = False);
 dp.remove_points(lambda x: x.is_interpolated_point, fix_durations = False, update_deco_info = True)
 
 print(dp.dive_summary());
