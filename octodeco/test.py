f7563086dd8633e49efd206c8debac8f7060b4d6
Dan Roozemond <dan.roozemond@gmail.com>
2020-05-23 14:21:59 +0200

    Check: works for case where 21m needs longer stop than 3 mins

--- octodeco/test.py
+++ octodeco/test.py
@@ -7,14 +7,14 @@ from octodeco.deco.DiveProfile import DiveProfile;
 
 t0 = time.perf_counter();
 
-dp = DiveProfile(gf_low=35, gf_high=65);
-dp.append_section(30, 33.5, gas = Gas.Air());
+dp = DiveProfile(gf_low=5, gf_high=65);
+dp.append_section(50, 50.5, gas = Gas.Air());
 dp.add_gas(Gas.Nitrox(50))
 dp.add_stops_to_surface();
 dp.interpolate_points()
 
 # dp.set_gf(0,0, updateStops = False);
-#dp.remove_points(lambda x: x.is_interpolated_point, fix_durations = False, update_deco_info = True)
+dp.remove_points(lambda x: x.is_interpolated_point, fix_durations = False, update_deco_info = True)
 
 print(dp.dive_summary());
 for s in dp.runtimetable():
