6f1d66700d225646c61355c019e12688c4b64ed1
Dan Roozemond <dan.roozemond@gmail.com>
2020-05-23 14:19:55 +0200

    fix deco_info on 1st point of diveprofile

--- octodeco/test.py
+++ octodeco/test.py
@@ -11,9 +11,10 @@ dp = DiveProfile(gf_low=35, gf_high=65);
 dp.append_section(30, 33.5, gas = Gas.Air());
 dp.add_gas(Gas.Nitrox(50))
 dp.add_stops_to_surface();
+dp.interpolate_points()
 
 # dp.set_gf(0,0, updateStops = False);
-dp.remove_points(lambda x: x.is_interpolated_point, fix_durations = False, update_deco_info = True)
+#dp.remove_points(lambda x: x.is_interpolated_point, fix_durations = False, update_deco_info = True)
 
 print(dp.dive_summary());
 for s in dp.runtimetable():
