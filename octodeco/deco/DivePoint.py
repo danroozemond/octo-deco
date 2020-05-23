c549aea63e7cf0118b26494b3a7904b51c95dd14
Dan Roozemond <dan.roozemond@gmail.com>
2020-05-23 13:50:25 +0200

    Refactor debug output into DivePoint.debug_info

--- octodeco/deco/DivePoint.py
+++ octodeco/deco/DivePoint.py
@@ -20,6 +20,18 @@ class DivePoint:
     def __repr__(self):
         return '%.1f:%dm' % (self.time, self.depth);
 
+    def debug_info(self):
+        fmt = '{}{}{} T {:6.1f}  D {:5.1f}  G {:5s}  DD {:5.1f}  FS {:5.1f}  ' +\
+               'C {:5.1f}   GF {:5.1f}/{:4.1f}   SGF {:6.1f}  {}';
+        return  fmt.format('I' if self.is_interpolated_point else ' ',\
+                      'D' if self.is_deco_stop else ' ',\
+                      'A' if self.is_ascent_point else ' ',\
+                        self.time, self.depth, str(self.gas), self.duration(),
+                        self.deco_info['FirstStop'],self.deco_info['Ceil'],
+                        self.deco_info['GF99'], self.deco_info['amb_to_gf'](self.p_amb),
+                        self.deco_info['SurfaceGF'],
+                        Util.stops_to_string_precise(self.deco_info['Stops']));
+
     @staticmethod
     def dataframe_columns_deco_info():
         return [ 'FirstStop', 'Ceil', 'Ceil99', 'GF99', 'SurfaceGF', 'LeadingTissueIndex', 'TTS', 'NDL' ];
--- octodeco/test.py
+++ octodeco/test.py
@@ -19,17 +19,5 @@ print(dp.dive_summary());
 for s in dp.runtimetable():
     print(s);
 
-for i in range(len(dp._points)):
-    p = dp._points[i];
-    if 36 < p.time < 46:
-        pp = dp._points[i-1];
-        # p.set_updated_deco_info(dp.deco_model(), dp._gases_carried, pp.deco_info['amb_to_gf']);
-        print('{}{}{} T {:6.1f}  D {:5.1f}  G {:5s}  DD {:5.1f}  FS {:5.1f}  C {:5.1f}   GF {:5.1f}/{:4.1f}   SGF {:6.1f}  ' \
-              .format('I' if p.is_interpolated_point else ' ',\
-                      'D' if p.is_deco_stop else ' ',\
-                      'A' if p.is_ascent_point else ' ',\
-                        p.time, p.depth, str(p.gas), p.duration(), p.deco_info['FirstStop'],p.deco_info['Ceil'],
-                        p.deco_info['GF99'], p.deco_info['amb_to_gf'](p.p_amb),
-                        p.deco_info['SurfaceGF']),
-              Util.stops_to_string_precise(p.deco_info['Stops']));
-
+for p in dp._points:
+    print(p.debug_info());
