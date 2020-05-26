be9ec01be41879c2a14a1ced0e5215a6b17a46c6
Dan Roozemond <dan.roozemond@gmail.com>
2020-05-26 12:02:41 +0200

    Including flaskr editing to change last stop

--- octodeco/flaskr/dive.py
+++ octodeco/flaskr/dive.py
@@ -349,6 +349,22 @@ def modify_meta(dive_id):
         abort(405);
 
 
+@bp.route('/modify/settings/<int:dive_id>', methods = [ 'POST' ])
+def modify_settings(dive_id):
+    if request.form.get('action_update_settings', '') != '':
+        # Some input sanitation
+        ipt_last_stop_depth = max(3, min(21, request.form.get('ipt_last_stop_depth', 3, type=int)));
+        dp = get_diveprofile_for_display(dive_id);
+        if ipt_last_stop_depth != dp._last_stop_depth:
+            dp._last_stop_depth = ipt_last_stop_depth;
+            dp.update_deco_info();
+            flash("Change last stop to %i m" % ipt_last_stop_depth);
+        db_dive.store_dive(dp);
+        _invalidate_cached_dive(dive_id);
+        return redirect(url_for('dive.show', dive_id=dive_id));
+    else:
+        abort(405);
+
 #
 # New (show the page, new from spec, new demo, new from
 #
