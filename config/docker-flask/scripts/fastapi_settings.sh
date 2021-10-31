a1fa8aa34e99bff193ece28aa8f391335a02b561
Dan Roozemond <dan.roozemond@gmail.com>
2021-10-31 22:01:21 +0100

    delete .sh that shouldn't be in repo

--- config/docker-flask/scripts/fastapi_settings.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Copy this file to fastapi_settings.sh and adjust accordingly.
-export DB_APPNAME=octodeco.db.simple.app:app
-export DB_PORT=8001
-export DB_SQLITE=/flask_instance/flaskr.sqlite
