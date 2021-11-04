427ab6b1341041779ec2d351534339c17415084f
Dan Roozemond <dan.roozemond@gmail.com>
2021-11-04 21:20:57 +0100

    wip (so far unsuccesful)

--- setup.py
+++ setup.py
@@ -6,15 +6,20 @@ from setuptools import setup, find_packages, Extension
 # PycharmProjects\octo-deco>python setup.py build_ext --inplace
 #
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 extensions = [ Extension("octodeco.deco.TissueStateCython", ["octodeco/deco/TissueStateCython.pyx"]) ];
 
 setup(
-    name="Octo-Deco",
-    version="2.0.0",
+    name="octo-deco",
+    version="2.0.1",
     packages=find_packages(),
     author="Dan Roozemond",
     author_email="dan.roozemond@gmail.com",
     description="Package to do scuba diving decompression profile calculations, including web app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://octo-deco.nl/",
     project_urls={
         "Source Code": "https://github.com/danroozemond/octo-deco/",
