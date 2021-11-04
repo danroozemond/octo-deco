5b5c1b4b1b505612bda41649ef294cd5732f8f6f
Dan Roozemond <dan.roozemond@gmail.com>
2021-11-04 09:48:49 +0100

    run_uwsgi_ini.dev.sh -> turn on auto reload for development

--- config/docker-flask/scripts/run_uwsgi_ini.dev.sh
+++ config/docker-flask/scripts/run_uwsgi_ini.dev.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+sleep 5s
 . $(dirname "$0")/flask_settings.sh
 env | grep FLASK
 cd /octo-deco
 pip install -e .
 cd -
-uwsgi $(dirname "$0")/uwsgi.ini 
+uwsgi $(dirname "$0")/uwsgi.ini --py-autoreload=1
