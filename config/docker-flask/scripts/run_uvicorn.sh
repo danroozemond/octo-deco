b628b7db5b50ddad889bed1c0f08acdfd2ea48df
Dan Roozemond <dan.roozemond@gmail.com>
2021-11-05 11:43:06 +0100

    SPEEEED

--- config/docker-flask/scripts/run_uvicorn.sh
+++ config/docker-flask/scripts/run_uvicorn.sh
@@ -2,6 +2,6 @@
 . $(dirname "$0")/fastapi_settings.sh
 env
 rm /usr/bin/X11/X11 # known issue with python 3.8 & uvicorn
-cd /octo-deco
+cd /octo-deco/octodeco/db
 uvicorn $DB_APPNAME --host 0.0.0.0 --port $DB_PORT  --log-level info
 
